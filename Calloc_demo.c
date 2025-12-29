58559df5f90592b3f2d9f21450ef092407306966
Gajanan Sunil Dimble <gajanandimble2355@gmail.com>
2025-12-29 10:23:00 +0530

    Add files via upload

---
+++ Calloc_demo.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int length=0;
+    int *Arr=NULL;
+
+    printf("Enter the number of elemenrs:\n");
+    scanf("%d",&length);
+
+    
+    Arr=(int *)calloc(length,sizeof(int));
+    if (Arr==NULL)
+    {
+        printf("Unable to allocate memory\n");
+
+    }
+    else
+    {
+        printf("Memory Gets succesfully allocated\n");
+    }
+    //Step 2:Use the memory
+    //Step 3: Free the memory
+    free(Arr);
+    return 0;
+}
---
+++ StaticDemo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int Arr[10];
+
+    printf("Static Memory Allocation\n");
+
+    printf("%d",sizeof(Arr));
+    
+    return 0;
+}
